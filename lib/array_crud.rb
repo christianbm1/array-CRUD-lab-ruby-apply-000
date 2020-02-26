1241709690d89e9e9b9861e1470579a26b599a05
Christian Martinez <christianbm1@gmail.com>
2020-02-26 21:28:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,12 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.
+  array.unshift(element)
+  return array
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = array.pop()
 end
 
 def remove_element_from_start_of_array(array)
